335dfef2250532329500b0f3d2002396e91ac9bb
camilla <camillaalves285@gmail.com>
2024-04-29 12:16:40 -0300

    Adicionando o Checkout

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -3,11 +3,31 @@
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import { CheckoutForm } from "@/components/checkout/checkout-form";
-//import { useCart } from "@/context/cart-context";
 
 const stripe = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!);
 
-const appearance = {};
+const appearance = {
+  rules: {
+    ".Input": {
+      borderRadius: "0.375rem",
+      paddingTop: "0.5rem",
+      paddingBottom: "0.5rem",
+      paddingLeft: "0.75rem",
+      paddingRight: "0.75rem",
+      lineHeight: "1.25rem",
+      borderColor: "#e2e8f0",
+      fontSize: ".875rem",
+    },
+    ".Label": {
+      fontWeight: "500",
+      color: "#050a17",
+      marginNBottom: "0.317rem",
+    },
+    ".Input:focus": {
+      boxShadow: "0 0 0 2px #fff, 0 0 0 4px #050a17",
+    },
+  },
+};
 
 export default function Checkout() {
   //const { total } = useCart();
--- src/components/checkout/checkout-form.tsx
+++ src/components/checkout/checkout-form.tsx
@@ -1,9 +1,66 @@
-import { AddressElement, PaymentElement } from "@stripe/react-stripe-js";
+import {
+  AddressElement,
+  PaymentElement,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js";
+
 import { CreditCard, LockKeyhole, MapPin } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { OrderSummary } from "./order-summary";
+import { FormEventHandler, useState } from "react";
+import { Product, useCart } from "@/context/cart-context";
+import { createClientSecret } from "./actions";
 
 export function CheckoutForm() {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const [loading, setLoading] = useState(false);
+  const { total, products } = useCart();
+
+  const createLineItems = (products: Product[]) => {
+    return products.map((product) => ({
+      name: product.name,
+      quantity: product.quantity,
+    }));
+  };
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    setLoading(true);
+
+    const { error: submitError } = await elements.submit();
+
+    if (submitError) {
+      console.log(submitError);
+      return;
+    }
+
+    const lineItems = createLineItems(products);
+
+    const clientSecret = await createClientSecret(total, lineItems);
+
+    const { error } = await stripe.confirmPayment({
+      elements,
+      clientSecret,
+      confirmParams: {
+        return_url: "http://localhost:3000/confirmation",
+      },
+    });
+
+    if (error) {
+      console.log(error);
+    }
+
+    setLoading(false);
+  };
+
   return (
     <main className="container mt-4">
       <div className="flex items-center gap-2 my-8">
@@ -11,7 +68,7 @@ export function CheckoutForm() {
         <h1 className="text-2xl font-semibold"> Finalize o seu pedido </h1>
       </div>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="flex gap-8">
           <div className="flex w-1/2 flex-col gap-8">
             <div className="w-full self-start rounded-lg border p-6">
@@ -30,7 +87,7 @@ export function CheckoutForm() {
               </h2>
               <PaymentElement />
 
-              <Button className="w-full" type="submit">
+              <Button className="w-full mt-8" type="submit" disabled={loading}>
                 Pagar
               </Button>
             </div>
